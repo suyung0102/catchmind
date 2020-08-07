6d654c220530ffc0d612b44b7c3a1c470ff2fd2a
suyung0102 <suyung0102@gmail.com>
2020-08-07 15:19:24 +0900

    2020/08/07 점수기능

--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,17 +1,30 @@
 import { handleNewUser, handleDisconnected } from "./notifications";
-import events from "../../src/events";
 import { handleNewMessage } from "./chat";
+import { handleBeganPath, handleStrokedPath, handleFilled } from "./paint";
+import {
+  handlePlayerUpdate,
+  handleGameStarted,
+  handleLeaderNotif,
+  handleGameEnded,
+  handleGameStarting,
+} from "./player";
 
 let socket = null;
 
 export const getSocket = () => socket;
 
-export const updateSocket = (aSocket) => (socket = aSocket);
-
 export const initSockets = (aSocket) => {
-  const { event } = window;
-  updateSocket(aSocket);
-  aSocket.on(events.newUser, handleNewUser);
-  aSocket.on(events.disconnected, handleDisconnected);
-  aSocket.on(events.newMsg, handleNewMessage);
+  const { events } = window;
+  socket = aSocket;
+  socket.on(events.newUser, handleNewUser);
+  socket.on(events.disconnected, handleDisconnected);
+  socket.on(events.newMsg, handleNewMessage);
+  socket.on(events.beganPath, handleBeganPath);
+  socket.on(events.strokedPath, handleStrokedPath);
+  socket.on(events.filled, handleFilled);
+  socket.on(events.playerUpdate, handlePlayerUpdate);
+  socket.on(events.gameStarted, handleGameStarted);
+  socket.on(events.leaderNotif, handleLeaderNotif);
+  socket.on(events.gameEnded, handleGameEnded);
+  socket.on(events.gameStarting, handleGameStarting);
 };
--- src/server.js
+++ src/server.js
@@ -25,4 +25,4 @@ const server = app.listen(PORT, handleListening);
 
 const io = socketIO.listen(server);
 
-io.on("connection", (socket) => socketController(socket));
+io.on("connection", (socket) => socketController(socket, io));
